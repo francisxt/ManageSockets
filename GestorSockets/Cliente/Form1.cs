cfae2585b125d3d559634f601f5f7027ceae124b
francisxt <francisxt@live.com>
2018-07-10 02:34:37 -0500

    Add files via upload

---
+++ GestorSockets/Cliente/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace GestorSockets
+{
+    public partial class frmCliente : Form
+    {
+        ConexionCliente gestorCliente = null;
+        byte[] bufferTx;
+        byte[] bufferRx = new byte[2048];
+        public frmCliente()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        public void EstablecerGestorCliente(ConexionCliente gestor)
+        {
+            this.gestorCliente = gestor;
+        }
+        public void ManejoLog(string msg)
+        {
+            txtLog.AppendText(msg);
+        }
+
+        private void btnActualizarBinario_Click(object sender, EventArgs e)
+        {
+            bufferTx = null; txtBinarioEnviar.Text = "";
+            String datosAEnviar = "";
+            int posicion = -1;
+            if (chkTexto.Checked && txtTextoAEnviar.Text.Length != 0)
+            {
+                if (txtTextoAEnviar.Text.IndexOf("\\n") != -1)
+                {
+                    String[] subs = txtTextoAEnviar.Text.Split(new String[] { "\\n" }, StringSplitOptions.None);
+                    for (int i = 0; i < subs.Length; i++)
+                    {
+                        if (subs[i].Equals(""))
+                            datosAEnviar += "\n";
+                        else datosAEnviar += subs[i];
+                    }
+                }
+                else
+                    datosAEnviar = txtTextoAEnviar.Text;
+                bufferTx = Encoding.ASCII.GetBytes(datosAEnviar);
+            }
+            else
+            {
+                if (txtTextoAEnviar.Text.Length >= 2)
+                {
+                    string delimitador = " ";
+                    byte resultado = 0x00; char[] numero;
+                    string[] cadenaDeDatos = txtTextoAEnviar.Text.Split(delimitador.ToCharArray());
+                    bufferTx = new byte[cadenaDeDatos.Length];
+                    for (int i = 0; i < cadenaDeDatos.Length; i++)
+                    {
+                        try
+                        {
+                            numero = cadenaDeDatos[i].ToCharArray();
+                            if (numero.Length != 2)
+                                throw new Exception("");
+                            byte.TryParse(numero[0].ToString(), out resultado);
+                            bufferTx[i] = (byte)(resultado << 4);
+                            byte.TryParse(numero[1].ToString(), out resultado);
+                            bufferTx[i] |= (byte)resultado;
+                        }
+                        catch (Exception ex)
+                        {
+                            gestorCliente.Traza("Hay un error en el formato, recuerda: si es binario, debes escribir, 12 34 AB, siendo estos numeros hexadecimales");
+                        }
+                    }
+                }
+            }
+
+            if (bufferTx != null) for (int i = 0; i < bufferTx.Length; i++)
+                {
+                    txtBinarioEnviar.AppendText(bufferTx[i].ToString("x") + " ");
+                }
+        }
+
+        private void btnResolver_Click(object sender, EventArgs e)
+        {
+            if (txtServidor.Text.Length > 0)
+            {
+                txtIPServidor.Text = gestorCliente.ObtenerDireccionIP(txtServidor.Text).ToString();
+            }
+            else gestorCliente.Traza("Por favor incluye el nombre del servidor");
+        }
+
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtIPServidor.Text.Length != 0 && txtServidor.Text.Length != 0 && txtPuerto.Text.Length != 0)
+                {
+                    gestorCliente.EspecificarServidor(txtServidor.Text);
+                    gestorCliente.EspecificarPuertoServidor(txtPuerto.Text);
+                    gestorCliente.Conectar();
+                }
+                else throw new Exception();
+            }
+            catch (Exception ex)
+            {
+                gestorCliente.Traza("Por favor comprueba la dirección IP o el nombre de servidor");
+            }
+        }
+        private void btnEnviar_Click(object sender, EventArgs e)
+        {
+            int recibidos = gestorCliente.EnviarRecibir(bufferTx, ref bufferRx);
+            gestorCliente.Traza("Recibidos: " + recibidos + " bytes");
+            txtRecibidoBinario.Text = "";
+            for (int i = 0; i < recibidos; i++)
+            {
+                txtRecibidoBinario.AppendText(bufferRx[i].ToString("X"));
+            }
+            String respuesta = Encoding.ASCII.GetString(bufferRx, 0, recibidos);
+            txtRespuesta.Text = "";
+            txtRespuesta.AppendText(respuesta);
+        }
+
+        private void btnProbar_Click(object sender, EventArgs e)
+        {
+            lblEstado.Text = gestorCliente.ObtenerResultadoPruebaConexion();
+        }
+
+    }
+}
---
+++ GestorSockets/Cliente/Program.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Diagnostics;
+using System.Text;
+using Cliente.Properties;
+
+namespace GestorSockets
+{
+    static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main() 
+        { 
+            ConexionCliente gestor = new ConexionCliente(new frmCliente()); 
+            ((frmCliente)gestor.ObtenerVista()).EstablecerGestorCliente(gestor); 
+            gestor.EspecificarLog(((frmCliente)gestor.ObtenerVista()).ManejoLog); 
+            Application.Run(gestor.ObtenerVista());
+
+            //Application.EnableVisualStyles(); 
+            //Application.SetCompatibleTextRenderingDefault(false); 
+            //Application.Run(new frmCliente()); 
+        }
+    }
+    public enum estadoConexion
+    {
+        ok = 0,
+        problemaDns = 1,
+        problemaConSocket = 2,
+        problemaConDns_y_Socket = 3,
+        errorDeRedOInalcanzable = 4
+    }  
+    public class ConexionCliente
+        {
+            TcpClient cliente = new TcpClient();
+            Int32 puerto;
+            IPAddress direccionServidor;
+            IPEndPoint sitioRemoto;
+            private estadoConexion ObtenerError(estadoConexion actual, estadoConexion siguiente)
+            {
+                Traza("Error Detectado");
+                if (actual == estadoConexion.ok)
+                    return siguiente;
+                if (actual == estadoConexion.problemaDns)
+                {
+                    if (siguiente == estadoConexion.problemaConSocket)
+                    {
+                        return estadoConexion.problemaConDns_y_Socket;
+                    }
+                    else
+                    {
+                        return siguiente;
+                    }
+                }
+                return siguiente;
+            }
+            public String ObtenerResultadoPruebaConexion()
+            {
+                return PruebaConexion().ToString();
+            }
+
+            private estadoConexion PruebaConexion()
+            {
+                MessageBox.Show("Prueba de Conexión de Cliente");
+                estadoConexion resultado = estadoConexion.ok;
+                String testHttp = "GET /index.html HTTP/1.0\n\n";
+                String httpDoc = null; int cantRecibida = 0;
+                Byte[] bytesParaEnviar = Encoding.ASCII.GetBytes(testHttp);
+                Byte[] bytesParaRecibir = new Byte[1024]; string nombre = "";
+                IPAddress IPPrueba = null; IPEndPoint extremoPrueba = null;
+                TcpClient clientePrueba = null;
+                try
+                {
+                    IPPrueba = Dns.GetHostEntry("www.epn.edu.ec").AddressList[0];
+                    extremoPrueba = new IPEndPoint(IPPrueba, 80);
+                }
+                catch (Exception ex)
+                {
+                    Traza("Parece que el DNS no funciona...");
+                    resultado = ObtenerError(resultado, estadoConexion.problemaDns);
+                }
+                try
+                {
+                    nombre = ((IPHostEntry)Dns.GetHostEntry("163.117.139.128")).HostName;
+                    TcpClient cliente = new TcpClient();
+                }
+                catch (Exception ex)
+                {
+                    Traza("Problemas con los sockets...");
+                    return ObtenerError(resultado, estadoConexion.problemaConSocket);
+                }
+                try
+                {
+                    cliente.Connect(extremoPrueba);
+                    NetworkStream flujo = cliente.GetStream();
+                    flujo.Write(bytesParaEnviar, 0, bytesParaEnviar.Length);
+                    cantRecibida = flujo.Read(bytesParaRecibir, 0, bytesParaRecibir.Length);
+                }
+                catch (Exception ex)
+                {
+                    Traza("Error en la conexión...");
+                    return ObtenerError(resultado, estadoConexion.errorDeRedOInalcanzable);
+                }
+                httpDoc = Encoding.ASCII.GetString(bytesParaRecibir, 0, cantRecibida);
+                Traza("Prueba finalizada"); return resultado;
+            }
+
+            public void EspecificarServidor(String direccionIP)
+            {
+                direccionServidor = Dns.GetHostEntry(direccionIP).AddressList[0];
+            }
+            public void EspecificarPuertoServidor(String puerto)
+            {
+                int resultado = 0;
+                try
+                {
+                    int.TryParse(puerto, out resultado);
+                    this.puerto = resultado;
+                }
+                catch (Exception ex)
+                {
+                    Traza("Está correcto el puerto?");
+                }
+            }
+            public IPAddress ObtenerDireccionIP(String nombreEquipo)
+            {
+                return Dns.GetHostAddresses(nombreEquipo)[0];
+            }
+            public void Conectar()
+            {
+                try
+                {
+                    if (cliente != null)
+                        if (cliente.Connected)
+                        {
+                            Traza("Cerrando conexiones...");
+                            cliente.Client.Disconnect(true);
+                        }
+                    Traza("Creando un endpoint...");
+                    sitioRemoto = new IPEndPoint(direccionServidor, puerto);
+                    Traza("Creando el socket...");
+                    cliente = new TcpClient();
+                    Traza("Conectando...");
+                    cliente.Connect(sitioRemoto);
+                }
+                catch (Exception ex)
+                {
+                    Traza("Error en la conexión " + ex.Message);
+                }
+            }
+
+            public int EnviarRecibir(byte[] buferTx, ref byte[] buferRx)
+            {
+                try
+                {
+                    int bytes_obtenidos = 0;
+                    NetworkStream flujo = cliente.GetStream();
+                    flujo.Write(buferTx, 0, buferTx.Length);
+                    bytes_obtenidos = flujo.Read(buferRx, 0, buferRx.Length);
+                    return bytes_obtenidos;
+                }
+                catch (SocketException sExec)
+                { Traza("Error: " + sExec.Message); }
+                return 0;
+            }
+            frmCliente formularioCliente = new frmCliente();
+            public ConexionCliente(frmCliente formularioCliente)
+            {
+                this.formularioCliente = formularioCliente;
+            }
+            ~ConexionCliente()
+            {
+                if (cliente != null)
+                {
+                    if (cliente.Connected) cliente.Close();
+                }
+            }
+            ManejoLog log;
+            public void EspecificarLog(ManejoLog log)
+            {
+                this.log = log;
+            }
+            public void Traza(string msg)
+            {
+                StackTrace traza = new StackTrace(false);
+                string metodoUsado = traza.GetFrame(1).GetMethod().Name;
+                log(metodoUsado + " : " + msg + "\r\n");
+            }
+            public Form ObtenerVista()
+            {
+                return formularioCliente;
+            }
+            public delegate void ManejoLog(string msg);
+        }       
+    
+}
